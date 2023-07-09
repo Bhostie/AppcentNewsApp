4f2769b7645958a61a42b5fe8a3ff456bc58544d
Barış Gökmen <barisgokmen06@gmail.com>
2023-07-09 14:29:40 +0300

    file location refactoring, MainActivity.kt moved to UI

--- app/src/main/java/com/appcentnewsapp/barisgokmen/ui/MainActivity.kt
+++ app/src/main/java/com/appcentnewsapp/barisgokmen/ui/MainActivity.kt
@@ -1,12 +1,11 @@
-package com.appcentnewsapp.barisgokmen
+package com.appcentnewsapp.barisgokmen.ui
 
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.appcentnewsapp.barisgokmen.R
 import com.appcentnewsapp.barisgokmen.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
